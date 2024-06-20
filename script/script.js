d4bef83904f99771572992da5171249a0cdd704f
selinekici <113785033+selinekici@users.noreply.github.com>
2024-06-20 17:09:48 +0300

    initial

---
+++ script/script.js
@@ -0,0 +1,303 @@
+
+$(function () {
+    var $products = $('#products');
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8001/products',
+        success: function (products) {
+
+            $.each(products, function (i, product) {
+                var contDiv = $('<div id="' + product._id + '">');
+                $(contDiv).click(function () {
+                        console.log('click');
+                        var prodDetail = $('<div>  </div>');
+                        var descriptionDiv = $('<div>  </div>').append('<strong>' + product.name + ' <small><br> ' + product.details + ' <br><strong>  ' +
+                            '$' + product.price + '<small><br> Manifacturer: ' + product.manufacturer);
+                        $(product.name).css('font-size', '30px');
+                        var btn = $('<button> Add Card </button>');
+                        var id = contDiv.attr('id');
+                        $.ajax({
+                            type: 'GET',
+                            url: 'http://localhost:8001/products/' + id,
+                            success: function (product) {
+                                var img = $('<img>').attr({
+                                    src: product.imageUrl,
+                                    width: 550, // Set the image width
+                                    height: 550 // Set the image height
+                                });
+                                prodDetail.append(img);
+                                img.css({'display': 'inline-block'});
+                                prodDetail.append(descriptionDiv);
+                                descriptionDiv.css({'margin-top': '20px', 'display': 'inline-block', 'font-size': '20px'});
+                                prodDetail.append(btn);
+                                prodDetail.css({'display': 'inline-block'});
+                                btn.css({
+                                    'background-color': '#CC2C2CFF',
+                                    'border-color': '#CC2C2CFF',
+                                    'border-radius': '25px',
+                                    'color': 'white',
+                                    'width': '150px',
+                                    'height': '50px',
+                                    'margin-top': '20px',
+                                    'display': 'inline-block'
+                                });
+                            }
+                        })
+                        $('#products-container').empty().append(prodDetail);
+                        $(window).scrollTop($('#products-container').offset().top);
+
+                    }
+                );
+
+                var img = $('<img>').attr({
+                    src: product.imageUrl,
+                    width: 250, // Set the image width
+                    height: 250 // Set the image height
+                });
+                var div = $('<div>').css({'font-size': '15px', 'text-align': 'center'})
+                    .append(product.name + ' <br><strong>  $' + product.price);
+                contDiv.append(img).append(div);
+                $products.append(contDiv);
+
+            });
+        }
+    });
+});
+
+$('#products').css("flex-wrap", "wrap");
+
+//register
+$('#registerButton').click(function () {
+    console.log("clicked");
+    var email = $('#registerEmail').val();
+    var password = $('#registerPassword').val();
+    var address = $('#registerAddress').val();
+    var firstname = $('#registerFirstname').val();
+    var lastname = $('#registerLastname').val();
+    var object = {email: email, password: password, address: address, firstname: firstname, lastname: lastname};
+    console.log(object);
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:8001/register',
+        data: JSON.stringify(object),
+        contentType: "application/json",
+        dataType: 'json',
+        success: function (response) {
+            alert("You created the account. You can go to log in.");
+            window.location.href = "login.html";
+        },
+        error: function (xhr, status, error) {
+            var errorMessage = xhr.responseJSON.message;
+            alert(errorMessage);
+        }
+    });
+});
+
+
+//login
+$('#loginButton').click(function () {
+    var email = $('#loginEmail').val();
+    var password = $('#loginPassword').val();
+    var login = {email: email, password: password}
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:8001/login',
+        data: JSON.stringify(login),
+        contentType: "application/json",
+        dataType: 'json',
+        success: function (response) {
+            alert("You are in the account successfully!!");
+            setData('user', JSON.stringify({email:email, isLoggedIn:true}));
+            console.log('getData:' + JSON.parse(getData('user')));
+            window.location.href = "index.html";
+
+        },
+        error: function (xhr, status, error) {
+            var errorMessage = "You have not an account, You can register.";
+            alert(errorMessage);
+        }
+    });
+});
+
+
+$('#logoutButton').click(function () {
+    var email = getEmail();
+    if(email){
+        console.log('logout clicked');
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost:8001/logout',
+            data: JSON.stringify({email:email}),
+            contentType: "application/json",
+            dataType: 'json',
+            success: function (response) {
+                clearData();
+                window.location.href = "index.html";
+            },
+            error: function (xhr, status, error) {
+                console.log('errors: '+status);
+                console.log('errors: '+error);
+            }
+        });
+    }
+});
+
+$(document).ready(isLoggedIn);
+
+function isLoggedIn(){
+    var user = getData('user');
+    console.log('user: ' + user);
+
+    if(user) {
+        user = JSON.parse(user);
+        if (user.isLoggedIn){
+            $('#logoutButton').show();
+        }
+
+    } else {
+        $('#logoutButton').hide();
+    }
+}
+
+function getEmail(){
+    var user = getData('user');
+    console.log('user: ' + user);
+
+    if(user) {
+        user = JSON.parse(user);
+        return user.email;
+
+    } else {
+        return null;
+    }
+}
+
+function getData(key) {
+    var data = localStorage.getItem(key);
+    if (data === null) {
+        return false;
+    } else {
+        return data;
+    }
+}
+
+function setData(key, value) {
+    if (key === undefined || value === undefined) {
+        return false;
+    } else {
+        localStorage.setItem(key, value);
+        return true;
+    }
+}
+
+function clearData() {
+    localStorage.clear();
+}
+
+$('#womanbtn').click(function () {
+    console.log("women clicked");
+    var $products = $('#products');
+    $products.empty();
+        $.ajax({
+            type: 'GET',
+            url: 'http://localhost:8001/products-by-category/women',
+            success: function (products) {
+                processProducts(products);
+            },
+            error: function (xhr, status, error) {
+                console.log("ERROR");
+            }
+        });
+});
+
+$('#manbtn').click(function () {
+    console.log("women clicked");
+    var $products = $('#products');
+    $products.empty();
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8001/products-by-category/man',
+        success: function (products) {
+            processProducts(products);
+        },
+        error: function (xhr, status, error) {
+            console.log("ERROR");
+        }
+    });
+});
+
+$('#kidsbtn').click(function () {
+    console.log("women clicked");
+    var $products = $('#products');
+    $products.empty();
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8001/products-by-category/kids',
+        success: function (products) {
+            processProducts(products);
+        },
+        error: function (xhr, status, error) {
+            console.log("ERROR");
+        }
+    });
+});
+function processProducts(products) {
+    var $products = $('#products');
+    $products.empty();
+
+    console.log("products; ", JSON.stringify(products));
+    $.each(products, function (i, product) {
+        var contDiv = $('<div id="' + product._id + '">');
+        $(contDiv).click(function () {
+                console.log('click');
+                var prodDetail = $('<div>  </div>');
+                var descriptionDiv = $('<div>  </div>').append('<strong>' + product.name + ' <small><br> ' + product.details + ' <br><strong>  ' +
+                    '$' + product.price + '<small><br> Manifacturer: ' + product.manufacturer);
+                $(product.name).css('font-size', '30px');
+                // var btn = $('<button> Add Card </button>');
+                var id = contDiv.attr('id');
+                $.ajax({
+                    type: 'GET',
+                    url: 'http://localhost:8001/products/' + id,
+                    success: function (product) {
+                        var img = $('<img>').attr({
+                            src: product.imageUrl,
+                            width: 550, // Set the image width
+                            height: 550 // Set the image height
+                        });
+                        prodDetail.append(img);
+                        img.css({'display': 'inline-block'});
+                        prodDetail.append(descriptionDiv);
+                        descriptionDiv.css({'margin-top': '20px', 'display': 'inline-block', 'font-size': '20px'});
+                        prodDetail.append(btn);
+                        prodDetail.css({'display': 'inline-block'});
+                        btn.css({
+                            'background-color': '#CC2C2CFF',
+                            'border-color': '#CC2C2CFF',
+                            'border-radius': '25px',
+                            'color': 'white',
+                            'width': '150px',
+                            'height': '50px',
+                            'margin-top': '20px',
+                            'display': 'inline-block'
+                        });
+                    }
+                })
+                $('#products-container').empty().append(prodDetail);
+                $(window).scrollTop($('#products-container').offset().top);
+
+            }
+        );
+
+        var img = $('<img>').attr({
+            src: product.imageUrl,
+            width: 250, // Set the image width
+            height: 250 // Set the image height
+        });
+        var div = $('<div>').css({'font-size': '15px', 'text-align': 'center'})
+            .append(product.name + ' <br><strong>  $' + product.price);
+        contDiv.append(img).append(div);
+        $products.append(contDiv);
+
+    });
+}
